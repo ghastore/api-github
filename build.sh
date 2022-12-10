1b11a11ca6bd0a9c5ccd505e7998221042962baa
KitsuneSolar <kitsune.solar@gmail.com>
2022-12-11 00:27:50 +0300

    2022-12-10 21:27:50

--- build.sh
+++ build.sh
@@ -13,12 +13,8 @@ API_DIR="${5}"
 API_OWNER="${6}"
 API_TOKEN="${7}"
 
-# Vars.
-TIME_MOD="+$(( 60*24 ))"
-
 # Apps.
 date="$( command -v date )"
-find="$( command -v find )"
 gh="$( command -v gh )"
 git="$( command -v git )"
 jq="$( command -v jq )"
@@ -70,18 +66,10 @@ api_org() {
   echo "--- [GITHUB] ORGANIZATION"
   _pushd "${d_src}" || exit 1
 
-  local u_org="orgs/${API_OWNER}"
-  local d_org="${API_DIR}/${API_OWNER}"
-  local f_org="${d_org}/${API_OWNER}.json"
+  local dir="${API_DIR}/${API_OWNER}"
+  [[ ! -d "${dir}" ]] && _mkdir "${dir}"
 
-  [[ ! -d "${d_org}" ]] && _mkdir "${d_org}"
-
-  # Org API.
-  if [[ ! -f "${f_org}" ]] || [[ $( ${find} "${f_org}" -mmin ${TIME_MOD} -print ) ]]; then
-    echo "Get API '${u_org}'..." && _gh "${u_org}" "${f_org}"
-  else
-    echo "File '${f_org}' is not changed!"
-  fi
+  _gh "orgs/${API_OWNER}" "${dir}/${API_OWNER}.json"
 
   _popd || exit 1
 }
@@ -94,34 +82,18 @@ api_repos() {
   echo "--- [GITHUB] REPOSITORIES"
   _pushd "${d_src}" || exit 1
 
-  local d_repos="${API_DIR}/${API_OWNER}/repos"
-  [[ ! -d "${d_repos}" ]] && _mkdir "${d_repos}"
+  local dir="${API_DIR}/${API_OWNER}/repos"
+  [[ ! -d "${dir}" ]] && _mkdir "${dir}"
 
   local repos
   readarray -t repos < <( _gh_list "orgs/${API_OWNER}/repos?type=public" ".[].name" )
 
   for repo in "${repos[@]}"; do
-    local u_repos="repos/${API_OWNER}/${repo}"
-    local u_readme="repos/${API_OWNER}/${repo}/readme"
-    local f_repo="${d_repos}/${repo}.json"
-    local f_readme="${d_repos}/${repo}.readme.json"
-
-    # Repos API.
-    if [[ ! -f "${f_repo}" ]] || [[ $( ${find} "${f_repo}" -mmin ${TIME_MOD} -print ) ]]; then
-      echo "Get API '${u_repos}'..." && _gh "${u_repos}" "${f_repo}"
-    else
-      echo "File '${f_repo}' is not changed!"
-    fi
-
-    # Readme API.
-    if [[ ! -f "${f_readme}" ]] || [[ $( ${find} "${f_readme}" -mmin ${TIME_MOD} -print ) ]]; then
-      echo "Get API '${u_readme}'..." && _gh "${u_readme}" "${f_readme}"
-    else
-      echo "File '${f_readme}' is not changed!"
-    fi
+    _gh "repos/${API_OWNER}/${repo}" "${dir}/${repo}.json"
+    _gh "repos/${API_OWNER}/${repo}/readme" "${dir}/${repo}.readme.json"
   done
 
-  ${jq} -nc '$ARGS.positional' --args "${repos[@]}" > "${d_repos%/*}/${API_OWNER}.repos.json"
+  ${jq} -nc '$ARGS.positional' --args "${repos[@]}" > "${dir%/*}/${API_OWNER}.repos.json"
 
   _popd || exit 1
 }
@@ -134,34 +106,18 @@ api_users() {
   echo "--- [GITHUB] USERS"
   _pushd "${d_src}" || exit 1
 
-  local d_users="${API_DIR}/${API_OWNER}/users"
-  [[ ! -d "${d_users}" ]] && _mkdir "${d_users}"
+  local dir="${API_DIR}/${API_OWNER}/users"
+  [[ ! -d "${dir}" ]] && _mkdir "${dir}"
 
   local users
   readarray -t users < <( _gh_list "orgs/${API_OWNER}/public_members" ".[].login" )
 
   for user in "${users[@]}"; do
-    local u_user="users/${user}"
-    local u_gpg="${u_user}/gpg_keys"
-    local f_user="${d_users}/${user}.json"
-    local f_gpg="${d_users}/${user}.gpg.json"
-
-    # Users API.
-    if [[ ! -f "${f_user}" ]] || [[ $( ${find} "${f_user}" -mmin ${TIME_MOD} -print ) ]]; then
-      echo "Get API '${u_user}'..." && _gh "${u_user}" "${f_user}"
-    else
-      echo "File '${f_user}' is not changed!"
-    fi
-
-    # GPG API.
-    if [[ ! -f "${f_gpg}" ]] || [[ $( ${find} "${f_gpg}" -mmin ${TIME_MOD} -print ) ]]; then
-      echo "Get API '${u_gpg}'..." && _gh "${u_gpg}" "${f_gpg}"
-    else
-      echo "File '${f_gpg}' is not changed!"
-    fi
+    _gh "users/${user}" "${dir}/${user}.json"
+    _gh "users/${user}/gpg_keys" "${dir}/${user}.gpg.json"
   done
 
-  ${jq} -nc '$ARGS.positional' --args "${users[@]}" > "${d_users%/*}/${API_OWNER}.users.json"
+  ${jq} -nc '$ARGS.positional' --args "${users[@]}" > "${dir%/*}/${API_OWNER}.users.json"
 
   _popd || exit 1
 }
